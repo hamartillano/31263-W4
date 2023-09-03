ca4f5d061214798f1ce4de9caa97b08b10ac19f6
hamartillano <hamartillano@gmail.com>
2023-09-03 15:17:31 +1000

    70% complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,10 +6,19 @@ public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private GameObject blueObj;
 
+    private void Start()
+    {
+        // Limit the frame rate to 60 FPS.
+        Application.targetFrameRate = 60;
+
+        // Instantiate the red prefab at (2, 0, 0) with no rotation.
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+
+        // Instantiate the blue prefab at (-2, 0, 0) with no rotation.
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
